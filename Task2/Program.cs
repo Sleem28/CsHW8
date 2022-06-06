266a3ae244d9a856a1d315c547972f4cbc69ea93
Kiryl Pashkin <sleembelbox@gmail.com>
2022-06-06 13:03:51 +0300

    modified Task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -7,24 +7,28 @@ int[,] GetFilledMatrix(int rows,int columnes)
 
     for(int i=0;i<matrix.GetLength(0);i++)
         for (int j=0; j<matrix.GetLength(1);j++)
-            matrix[i,j] = rnd.Next(0,1000);
+            matrix[i,j] = rnd.Next(0,100);
             
     return(matrix);    
 }
 //------------------------------------------------------------------------------------------------------------------------------+
 void PrintIntMatrix(int[,] matrix)
 {
+    string backSpaces = string.Empty;
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
-            Console.Write($"{matrix[i, j]} ");
+        {
+            backSpaces = matrix[i,j]/10 == 0? "  ": " ";
+            Console.Write(matrix[i,j] + backSpaces);
+        }   
         Console.WriteLine();
     }
 }
 //------------------------------------------------------------------------------------------------------------------------------+
-int[,] GetCoordinatesMinValue(int[,] matrix)
+int[] GetCoordinatesMinValue(int[,] matrix)
 {
-    int[,] coordinatesMinValue = new int[1,2];
+    int[] coordinatesMinValue = new int[2];
     int    minValue            = 2000;
 
     for(int i=0;i<matrix.GetLength(0);i++)
@@ -33,42 +37,56 @@ int[,] GetCoordinatesMinValue(int[,] matrix)
                 if(matrix[i,j] < minValue)
                 {
                   minValue =  matrix[i,j];
-                  coordinatesMinValue[0,0] = i;
-                  coordinatesMinValue[0,1] = j;
+                  coordinatesMinValue[0] = i;
+                  coordinatesMinValue[1] = j;
                 }  
             }
     return(coordinatesMinValue);
 }
 //------------------------------------------------------------------------------------------------------------------------------+
-int[,] DeleteRowAndColumn(int[,] matrix, int row,int column)
+int[,] DeleteRowAndColumn(int[,] matrix,int row,int column)
 {
+    int[,] matrixResized = new int[matrix.GetLength(0)-1,matrix.GetLength(1)-1];
+    int rowCounter = 0;
     for(int i=0;i<matrix.GetLength(0);i++)
-        for (int j=0; j<matrix.GetLength(1);j++)
+        {
+             int colCounter = 0;
+           for (int j=0; j<matrix.GetLength(1);j++)
             {
-                if(i==row || j==column)
-                  matrix[i,j] = 0;
-            }
-    return(matrix);
+                if(j==column)
+                    continue;
+    
+                matrixResized[rowCounter,colCounter] = matrix[i,j];
+                colCounter++;
+            } 
+            if(i==row)
+                continue;
+            rowCounter++;
+        }
+    return(matrixResized);
 }
 //------------------------------------------------------------------------------------------------------------------------------+
 int    matrixRows     = 10;
 int    matrixColumnes = 10;
-int[,] coordinatesMinValue = new int[1,2];
+int[] coordinatesMinValue = new int[2];
 
 Console.WriteLine("\nПолучим матрицу с заполненными значениями.");
-int[,] matrix    = GetFilledMatrix(matrixRows,matrixColumnes);
+int[,] matrix        = GetFilledMatrix(matrixRows,matrixColumnes);
+int[,] matrixResized = new int[matrixRows-1,matrixColumnes-1];
 PrintIntMatrix(matrix);
 
 Console.WriteLine("\nНайдем координаты элемента с минимальным значением.");
 coordinatesMinValue = GetCoordinatesMinValue(matrix);
 
-int minValueInMatrix = matrix[coordinatesMinValue[0,0],coordinatesMinValue[0,1]];
-Console.WriteLine($"Минимальный элемент в матрице равен {minValueInMatrix}. Координаты в массиве: ряд {coordinatesMinValue[0,0]+1}, колонка {coordinatesMinValue[0,1]+1}.");
+int minValueInMatrix = matrix[coordinatesMinValue[0],coordinatesMinValue[1]];
+Console.WriteLine($"Минимальный элемент в матрице равен {minValueInMatrix}. Координаты в массиве: ряд {coordinatesMinValue[0]+1}, колонка {coordinatesMinValue[1]+1}.");
 
 Console.WriteLine("\nУдалим строку и столбец на пересечении с наименьшим элементом.");
-matrix = DeleteRowAndColumn(
-                            matrix: matrix,
-                            row:    coordinatesMinValue[0,0],
-                            column: coordinatesMinValue[0,1]
+matrixResized = DeleteRowAndColumn(
+                            matrix:        matrix,
+                            row:           coordinatesMinValue[0],
+                            column:        coordinatesMinValue[1]
                             );
-PrintIntMatrix(matrix);
+
+Console.WriteLine("/nМатрица после удаления строки и столбца.");
+PrintIntMatrix(matrixResized);
